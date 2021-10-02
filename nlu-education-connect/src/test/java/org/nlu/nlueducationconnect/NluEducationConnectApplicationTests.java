08503f8e24d4bf7ef520f7a20d3a3770d1da0d70
v-ppentescu <v-ppentescu@expediagroup.com>
2021-10-01 18:34:20 -0700

    initializing spring boot

---
+++ nlu-education-connect/src/test/java/org/nlu/nlueducationconnect/NluEducationConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nlu.nlueducationconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NluEducationConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
