8a96faa42a4b4264daee3a5d370f07d2640caf20
v-ppentescu <v-ppentescu@expediagroup.com>
2021-10-02 12:33:38 -0700

    we got auth now

--- nlu-ascender/src/App.js
+++ nlu-ascender/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator } from '@aws-amplify/ui-react'
 
 import Amplify from "aws-amplify";
 import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
+
 
 function App() {
   return (
@@ -26,4 +28,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App)
